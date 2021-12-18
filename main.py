ef379709983b53719984d8f8539f211662e0fe02
varjaktcd <90612189+varjaktcd@users.noreply.github.com>
2021-12-18 18:56:13 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,21 @@
+import json #for dict to string
+
 from github import Github
 
-#access token. I'm using a second Github account for
-#this module so I don't mind including it
-#in source code
+input = input("Please login by entering your Github access token:")
 
-token = Github("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")
+token = Github(input)
 #have the user enter their access token
 
 #get user from token
 user = token.get_user()
 
-#print user info
-print("user:  " + user.login)
+#dictionary
+dct = {'user': user.login,
+       'fullname': user.name,
+       'location': user.location,
+       'company' : user.company
+}
 
-if user.name is not None:
-    print("fullname: " + user.name)
+print ("dictionary: " + json.dumps(dct))
 
-if user.location is not None:
-    print("location: " + user.location)
-
-if user.company is not None:
-    print("company: " + user.company)
