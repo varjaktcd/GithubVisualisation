052e8e691681b785ae24cb4315f5d9c00370c1c9
varjaktcd <90612189+varjaktcd@users.noreply.github.com>
2021-12-18 22:00:53 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import json #for dict to string
-
+import pymongo
 from github import Github
 
 input = input("Please login by entering your Github access token:")
@@ -17,5 +17,24 @@ dct = {'user': user.login,
        'company' : user.company
 }
 
+
+#store dct in mongodb
+
+#remove null fields. shouldnt include null fields in mongo db's
+
+#this should be in a unit test function
+for k,v in dict(dct).items():
+       if v is None:
+              del dct[k]
+
 print ("dictionary: " + json.dumps(dct))
 
+#establish connection
+conn ="mongodb://localhost:27017"
+#this port matches the one in docker-compose.yml
+client = pymongo.MongoClient(conn)
+
+#create database
+db = client.classDB
+
+db.githubuser.insert_many([dct])
